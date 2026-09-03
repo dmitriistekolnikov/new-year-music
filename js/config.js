8872780617b20315b63717f5eaa7b6c7eb7cc258
Intel-Core-i7-3770 <dimastekolnikov13@gmail.com>
2026-09-03 18:08:03 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,24 +1,4 @@
-// === КОНФИГУРАЦИЯ ПРОЕКТА ===
-
-const API_URL = 'https://newyear.dimastekolnikov1.workers.dev/api';
-const TRACKS_COUNT = 12; // Количество файлов в папке music/
-
-// ЗАПАСНОЙ ПЛЕЙЛИСТ (используется, если не удалось прочитать метаданные MP3)
-// Отредактируй этот список под названия своих файлов!
-const FALLBACK_TRACKS = [
-    { title: "Jingle Bells Phonk", artist: "wi$$$uri" },
-    { title: "Last Christmas", artist: "Wham!" },
-    { title: "All I Want for Christmas", artist: "Mariah Carey" },
-    { title: "Snowman", artist: "Sia" },
-    { title: "Let It Snow", artist: "Dean Martin" },
-    { title: "Winter Wonderland", artist: "Michael Bublé" },
-    { title: "Santa Tell Me", artist: "Ariana Grande" },
-    { title: "Underneath the Tree", artist: "Kelly Clarkson" },
-    { title: "Merry Christmas", artist: "Ed Sheeran" },
-    { title: "Christmas Tree Farm", artist: "Taylor Swift" },
-    { title: "White Christmas", artist: "Bing Crosby" },
-    { title: "Happy New Year", artist: "ABBA" }
-];
+const TRACKS_COUNT = 12;
 
 const PREDICTIONS = [
     "🎄 В новом году тебя ждет невероятный успех!",
@@ -39,6 +19,5 @@ const BULB_COLORS = ['#ef4444', '#22c55e', '#38bdf8', '#fbbf24', '#f472b6'];
 let TRACKS = [];
 
 function getTrackUrl(fileNumber) {
-    // Убедись, что папка music лежит в корне репозитория
     return `/music/${fileNumber}.mp3`;
 }
