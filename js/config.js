4a9e58d8ba06fed965b696c2e45ddcecb9233535
Intel-Core-i7-3770 <dimastekolnikov13@gmail.com>
2026-09-03 22:04:31 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,23 +1,4 @@
-// === КОНФИГУРАЦИЯ ПРОЕКТА ===
-
-const TRACKS_COUNT = 12; // Сколько файлов 1.mp3, 2.mp3 и т.д. в папке music/
-
-// ЗАПАСНОЙ ПЛЕЙЛИСТ: Если jsmediatags не сможет прочитать файл, будут использованы эти названия.
-// Отредактируй их под свою музыку!
-const FALLBACK_TRACKS = [
-    { title: "Jingle Bells Phonk", artist: "wi$$$uri" },
-    { title: "Last Christmas", artist: "Wham!" },
-    { title: "All I Want for Christmas", artist: "Mariah Carey" },
-    { title: "Snowman", artist: "Sia" },
-    { title: "Let It Snow", artist: "Dean Martin" },
-    { title: "Winter Wonderland", artist: "Michael Bublé" },
-    { title: "Santa Tell Me", artist: "Ariana Grande" },
-    { title: "Underneath the Tree", artist: "Kelly Clarkson" },
-    { title: "Merry Christmas", artist: "Ed Sheeran" },
-    { title: "Christmas Tree Farm", artist: "Taylor Swift" },
-    { title: "White Christmas", artist: "Bing Crosby" },
-    { title: "Happy New Year", artist: "ABBA" }
-];
+const TRACKS_COUNT = 12;
 
 const PREDICTIONS = [
     "🎄 В новом году тебя ждет невероятный успех!",
@@ -32,11 +13,11 @@ const PREDICTIONS = [
 
 const THEMES = ['', 'theme-party', 'theme-winter'];
 const PARTICLE_COLORS = ['#fbbf24', '#ef4444', '#22c55e', '#ffffff', '#38bdf8', '#f472b6'];
-const SNOWFLAKES_CHARS = ['❅', '❆', '✻', '•']; // Пустые строки удалены
+const SNOWFLAKES_CHARS = ['❅', '❆', '✻', '•'];
 const BULB_COLORS = ['#ef4444', '#22c55e', '#38bdf8', '#fbbf24', '#f472b6'];
 
 let TRACKS = [];
 
 function getTrackUrl(fileNumber) {
-    return `/music/${fileNumber}.mp3`;
-    }
+    return `music/${fileNumber}.mp3`;
+}
