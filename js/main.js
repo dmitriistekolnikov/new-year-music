7b46c1ea848509956d5aaebe6f84ec4e7c7530c2
Intel-Core-i7-3770 <dimastekolnikov13@gmail.com>
2026-09-04 18:44:08 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,7 +19,6 @@ document.addEventListener('DOMContentLoaded', async () => {
     
     initCustomCursor();
     initParallax();
-    initTreeCounter();
     initSparkWaterfall();
     initElementTransforms();
     initNameFirework();
